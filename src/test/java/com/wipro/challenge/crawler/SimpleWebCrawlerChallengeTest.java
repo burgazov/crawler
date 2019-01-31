ef26c00844dd80095299fb6f7701c2e78259b76e
ferdinand.burgazov <ferdinand.burgazov@PROBOOK.guest>
2019-01-31 02:47:31 +0000

    Smoke test

---
+++ src/test/java/com/wipro/challenge/crawler/SimpleWebCrawlerChallengeTest.java
@@ -0,0 +1,61 @@
+package com.wipro.challenge.crawler;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.wipro.challenge.crawler.controller.CrawlerController;
+
+/**
+ * Simple smoke test
+ * @author ferdinand.burgazov
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleWebCrawlerChallengeTest {
+
+	@Autowired
+    private CrawlerController controller;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void contextLoads() {
+		assertNotNull( controller );
+	}
+}
